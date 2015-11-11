d2ef7c52b5661904a6b42fd6810a5c9dc3f30cda
yuzit <727760474@qq.com>
2015-11-12 01:27:07 +0800

    test github

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // test git
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
