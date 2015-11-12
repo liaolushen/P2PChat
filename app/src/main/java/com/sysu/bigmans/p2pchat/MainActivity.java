b77ccc1ecaae780bb5f16646e4c8f8b6cbcb518f
yuzit <727760474@qq.com>
2015-11-12 10:45:01 +0800

    add all

--- app/src/main/java/com/sysu/bigmans/p2pchat/MainActivity.java
+++ app/src/main/java/com/sysu/bigmans/p2pchat/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // test git
+        // test git d
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
